69ae3a78303559665a73b07aea18b71f4d8f7c1a
AxelBattigelli <axel.battigelli@epitech.eu>
2025-12-05 00:36:40 +0100

    feat: style & base page

---
+++ src/pages/About.tsx
@@ -0,0 +1,21 @@
+import '../style/Generic.css';
+
+export function AboutPage() {
+  return (
+    <div className="container">
+      <h1 className="title">À propos</h1>
+
+      <p className="text">
+        Bienvenue dans cette application !  
+        Ce projet a été conçu pour vous permettre de mieux comprendre les enjeux,
+        les possibiltés de redonner vie ou prolonger le numérique existant, le
+        tout dans une atmosphère inspirée des vieilles consoles.
+      </p>
+
+      <p className="text">
+        L'objectif est de montrer comment il est possible d'économiser des ressources,
+        de comprendre l'environement qui nous entoure et le prolonger.
+      </p>
+    </div>
+  );
+}
